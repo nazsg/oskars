1b2a6d06f15199cf6dff109f1f9e0d4d9db81b1b
Naz Sharid <post@nazs.net>
2020-06-23 13:11:16 +0100

    started antispam

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -10,6 +10,9 @@
       <div class="group g2">
         <label for="">Message</label> <textarea name="" id="" cols="30" rows="10" v-model="message"></textarea>
       </div>
+      <div class="antispam">
+        <antispam v-if="result == 'unverified'" result="unverified" v-on:updateResult="result = arguments[0]" />
+      </div>
       <div class="group actions">
         <SendIcon :size=size /> <DeleteIcon :size=size @click="clear"/>
       </div>
@@ -21,13 +24,14 @@
 <script>
 import SendIcon from 'vue-material-design-icons/EmailSend.vue'
 import DeleteIcon from 'vue-material-design-icons/TrashCanOutline.vue'
+import antispam from '~/components/antispam'
 export default {
-  components: { SendIcon, DeleteIcon },
+  components: { SendIcon, DeleteIcon, antispam },
   layout: 'blue',
   data() {
     return {
       name: '', email: '', message: '',
-      size: 40
+      size: 40, result: 'unverified'
     }
   },
   methods: {
